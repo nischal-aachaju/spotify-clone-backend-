b8e3a6975c2032715d40670da6f41162fe3f0580
Nischal Shrestha <sthanischal5060@gmail.com>
2026-02-16 21:04:49 +0545

    creating middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken")
+
+async function authArtist(req, res, next) {
+    const token = req.cookies.token
+    if (!token) {
+        return res.status(401).json({ message: "unauthorized" })
+    }
+
+    try {
+        const decode = jwt.verify(token, process.env.JWT_SECRET)
+        if (decode.role !== "artist") {
+           return res.status(403).json({ message: "you dont have access"})
+        }
+
+        next()
+    }
+
+    catch (err) {
+        console.log(err);
+        return res.status(401).json({
+            message: "unauthorized"
+        })
+    }
+}
+
+module.exports=authArtist
