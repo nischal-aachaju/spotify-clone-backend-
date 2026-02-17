95d484012bd0b2912a4b629647d4338052f2fc1c
Nischal Shrestha <sthanischal5060@gmail.com>
2026-02-17 14:00:45 +0545

    login feature

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,12 +2,21 @@ const express = require("express");
 const cookieParer = require("cookie-parser");
 const authRoutes = require("./routes/auth.routes");
 const musicRoutes = require("./routes/music.routes");
-
+const cors=require("cors")
 
 const app = express();
 app.use(express.json());
 app.use(cookieParer());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        message:"Hello backend"
+    })
+})
 app.use("/api/auth", authRoutes);
 app.use("/api/music", musicRoutes);
 
